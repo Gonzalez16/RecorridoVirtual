0d4e6c00bec49689c73e9df3dc489087ff2b3dc0
EduardoBarrAgu <barrancoaguilareduardo@gmail.com>
2022-10-31 21:44:23 -0600

    Update index.js

--- TourTec/index.js
+++ TourTec/index.js
@@ -527,5 +527,16 @@
 
   // Display the initial scene.
   switchScene(scenes[2]);
-
+  document.getElementById("boton").addEventListener("click", function(){
+    const name = document.getElementById("formulario").value.toLowerCase();
+    const vista = scenes.find( scene => scene.data.id.toLowerCase() === name );
+    if(vista == undefined) {
+      alert('No encontrado');
+    }else{
+      /*alert('Encontrado');
+      console.log(vista);*/
+      //findSceneDataById(switchScene(name));
+      switchScene(vista);
+    }
+  });
 })();
